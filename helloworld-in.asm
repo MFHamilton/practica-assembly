bf468ce1cf02c1c5e78ad741caf53530bf0fd7b8
MFHamilton <mariahamiltonr1@gmail.com>
2024-11-30 08:18:47 -0500

    Leccion #6

--- helloworld-in.asm
+++ helloworld-in.asm
@@ -1,18 +1,18 @@
-%include        'functions.asm'                             ; include our external file
+%include        'functions.asm'
  
 SECTION .data
-msg1    db      'Hello, brave new world!', 0Ah              ; our first message string
-msg2    db      'This is how we recycle in NASM.', 0Ah      ; our second message string
+msg1    db      'Hello, brave new world!', 0Ah, 0h          ; NOTE the null terminating byte
+msg2    db      'This is how we recycle in NASM.', 0Ah, 0h  ; NOTE the null terminating byte
  
 SECTION .text
 global  _start
  
 _start:
  
-    mov     eax, msg1       ; move the address of our first message string into EAX
-    call    sprint          ; call our string printing function
+    mov     eax, msg1
+    call    sprint
  
-    mov     eax, msg2       ; move the address of our second message string into EAX
-    call    sprint          ; call our string printing function
+    mov     eax, msg2
+    call    sprint
  
-    call    quit 
+    call    quit
